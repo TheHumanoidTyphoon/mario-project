257f087bed53b1b4074adc63ceda7fc4fcb394c0
TheHumanoidTyphoon <lukbr25@gmail.com>
2023-04-05 19:14:57 -0300

    some small changes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app.app import app
 from app.views import index, details, not_found_404
 
-app.add_url_rule('/', view_func=index)
-app.add_url_rule('/details.html', view_func=details)
-app.add_url_rule('/404.html', view_func=not_found_404)
+app.add_url_rule('/', index)
+app.add_url_rule('/details.html', details)
+app.add_url_rule('/404.html', not_found_404)
--- app/views.py
+++ app/views.py
@@ -1,12 +1,16 @@
 from flask import render_template
+from app.app import app
 
+@app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/details')
 def details():
     return render_template('details.html')
 
+@app.route('/404')
 def not_found_404():
-    return render_template('404.html')
+    return render_template('error handles/404.html')
 
 
